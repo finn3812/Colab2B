bd073717b6b743019160a2c612dca31aca3e57a4
finn3812 <51744153+finn3812@users.noreply.github.com>
2025-03-18 11:00:35 +0100

    Gør klar til github demo

--- Assets/NewBehaviourScript.cs
+++ Assets/NewBehaviourScript.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class NewBehaviourScript : MonoBehaviour
 {
     // Start is called before the first frame update
+    int githubTest = 0;
     void Start()
     {
         
