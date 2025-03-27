d9ee80943d97db0d0ab4697e286a9006a06ae5b8
SamuelKanBongo <Allahdinmor0@gmail.com>
2025-03-27 09:34:06 +0100

    Currency_Script

--- Assets/Undersystemmer/Currency/scripts/Currency.cs
+++ Assets/Undersystemmer/Currency/scripts/Currency.cs
@@ -1,18 +1,43 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class Currency : MonoBehaviour
 {
+    public TMP_Text GuldMængde; 
+    public int Penge;
+    public int MønteMængde;
     // Start is called before the first frame update
     void Start()
     {
-        
+        GuldMængde.text = Penge + " Guld";
     }
 
+    void SamlMønt()
+    {
+        MønteMængde = Random.Range(1, 17);
+        Debug.Log("Dødspengene er " + MønteMængde);
+    }
+    void GetMoney()
+    {
+       GuldMængde.text = Penge + " Guld";
+    }
     // Update is called once per frame
     void Update()
     {
-        
+    if (Input.GetKeyDown(KeyCode.G))
+    {
+        Penge=Penge+2;
+        GetMoney();
+        Debug.Log("knaptryk er trykket");
+    }
+        if (Input.GetKeyDown(KeyCode.U))
+        {
+            SamlMønt();
+            Penge = Penge + MønteMængde;
+            GetMoney();
+            Debug.Log("knaptryk er trykket");
+        }
     }
 }
