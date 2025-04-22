418badfa0532941cb8440e90c8a2a9d3f81f8d1d
RasmusvWerk <rasmusvwerkhoven@icloud.com>
2025-04-22 13:05:56 +0200

    Animation controller

--- Assets/Undersystemmer/Animation/Animation controller.cs
+++ Assets/Undersystemmer/Animation/Animation controller.cs
@@ -3,50 +3,25 @@ using System.Collections.Generic;
 using UnityEngine;
 
 
-public class PlayerMovement3D : MonoBehaviour
+public class Anim : MonoBehaviour
 {
-    public float moveSpeed = 5f;
-    public float rotationSpeed = 10f;
-
-    private Animator animator;
-    private Rigidbody rb;
-
-    private Vector3 movement;
-
-    void Start()
+    public enum Animtype
     {
-        animator = GetComponent<Animator>();
-        rb = GetComponent<Rigidbody>();
+        Idle,
+        Roam,
+        Chase, 
+        Attack,
+        Die
     }
 
-    void Update()
+    public void setAnimation(Animtype State)
     {
-        // Input
-        float moveX = Input.GetAxisRaw("Horizontal");
-        float moveZ = Input.GetAxisRaw("Vertical");
-
-        // Bevægelsesretning i verden
-        movement = new Vector3(moveX, 0f, moveZ).normalized;
-
-        // Opdater animatorens "Speed" parameter
-        animator.SetFloat("Speed", movement.magnitude);
-    }
-
-    void FixedUpdate()
-    {
-        // Flyt spilleren
-        Vector3 movePosition = rb.position + movement * moveSpeed * Time.fixedDeltaTime;
-        rb.MovePosition(movePosition);
-
-        // Roter spilleren i bevægelsesretningen
-        if (movement != Vector3.zero)
+        switch (State)
         {
-            Quaternion targetRotation = Quaternion.LookRotation(movement, Vector3.up);
-            Quaternion smoothedRotation = Quaternion.Slerp(transform.rotation, targetRotation, rotationSpeed * Time.fixedDeltaTime);
-            rb.MoveRotation(smoothedRotation);
+            case Animtype.Idle:
+                
         }
     }
-}
 
-// Start is called before the first frame update
+}
 
