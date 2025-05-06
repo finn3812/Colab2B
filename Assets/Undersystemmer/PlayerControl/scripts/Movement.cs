2c94d9507dd123c5b56c0db09ebd7b339ecc5774
trix-dk <sebastian.roemsgaard@gmail.com>
2025-05-06 12:52:04 +0200

    forsøg om at fixe fejl

--- Assets/Undersystemmer/PlayerControl/scripts/Movement.cs
+++ Assets/Undersystemmer/PlayerControl/scripts/Movement.cs
@@ -6,6 +6,7 @@ using UnityEngine;
 [RequireComponent(typeof(CharacterController))]
 public class Movement : MonoBehaviour
 {
+    // sebastian commit
     // --- Inspector Fields ---
     [Header("Dependencies")]
     [SerializeField] StaminaSystem staminaSystem;
